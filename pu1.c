cfd8511e01a6b56bd60e2f3bab2f9fc6398ef387
Valters S <snevalters@gmail.com>
2020-10-20 23:02:10 +0300

    PU1

---
+++ pu1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+ 
+int main()
+{
+ double a, b, reiz;
+ printf("Ievadiet vienu skaitli:  ");
+ scanf("%lf" ,&a);
+ printf("Ievadiet otro skaitli:  ");
+ scanf("%lf" ,&b);
+ reiz = a * b;
+ printf("Reizinājums ir %.0lf \n" , reiz);
+
+
+return 0;
+
+}
--- second.c
+++
@@ -1,8 +0,0 @@
-// Komentāra 1.rinda
-
-main(){}
-/*
-Komentāra 
-bloks
-*/
-
