6f3750e74ddb3b78c7d408352fbe312be1f230ea
valterssne <snevalters@gmail.com>
2020-11-08 21:29:58 +0200

    Create PU5.c
    same way as a while loop

---
+++ PU5.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+
+int n = 1;
+long long fakt = 1;
+int veids;
+long long int sk;
+
+int main()
+{
+    printf("Lūdzu, ievadiet decimālu skaitli, kam aprēķināt faktoriālu!\n");
+    scanf("%lld" ,&sk);
+    printf("Kādu datu tipi vēlaties izmantot? Ievadiet 1 - char, 2 - int, 3 - long long datu tipam!\n");
+    scanf("%d" ,&veids);
+    if(sk<0)
+    {
+        printf("Faktoriāls neeksistē šim skaitlim!\n");
+    }
+    else{
+            if(veids == 1)
+            {
+                if(sk>5)
+                {
+                    printf("Nevar aprēķināt faktoriālu ar izvēleto datu tipu!!!\n");
+                }
+                
+                else
+                    {
+                    for(sk; n<sk+1; n++)
+                        {
+                        fakt*=n;
+                        }
+                    printf("Faktoriālas no skaitļa %lld ir %lld.\n" , sk, fakt);
+                    }
+            }        
+            
+            else if(veids == 2)
+            {
+                if(sk>12)
+                {
+                    printf("Nevar aprēķināt faktoriālu ar izvēleto datu tipu!!!\n");
+                }
+                    
+                else{
+                    for(sk; n<sk+1; n++)
+                        {
+                        fakt*=n;
+                        }
+                    printf("Faktoriālas no skaitļa %lld ir %lld.\n" , sk, fakt);
+                    }
+           } 
+            
+            else if(veids == 3)
+           {
+                if(sk>20)
+                {
+                    printf("Nevar aprēķināt faktoriālu ar izvēleto datu tipu!!!\n");
+                }
+                    
+                else{
+                    for(sk; n<sk+1; n++)
+                        {
+                        fakt*=n;
+                        }
+                    printf("Faktoriālas no skaitļa %lld ir %lld.\n" , sk, fakt);
+                    }
+           }
+        }
+        
+return 0;
+}
